5ee72f3a7fbe8656375b85f45254da30b65813c0
SalsabilaHanaPradipta <a710230057@student.ums.ac.id>
2024-06-07 15:12:53 +0700

    Create day2tipedata.py
    day2tipedata

---
+++ day2/day2tipedata.py
@@ -0,0 +1,13 @@
+# Program untuk mengkonversi jarak dalam kilometer menjadi meter
+
+# Meminta input dari user berupa jarak dalam kilometer
+kilometer = input("Masukkan jarak dalam kilometer: ")
+
+# Mengubah input dari user menjadi tipe int
+kilometer = int(kilometer)
+
+# Menghitung jarak dalam meter
+meter = kilometer * 1000
+
+# Menampilkan output berupa jarak dalam meter
+print(f"Jarak dalam meter: {meter} meter")
