9abcc4bb6ab659c75da1c85a4bda53a3c26fec7d
SalsabilaHanaPradipta <a710230057@student.ums.ac.id>
2024-06-08 11:53:45 +0700

    Create day3string.py
    day3string

---
+++ day3/day3string.py
@@ -0,0 +1,24 @@
+# Program Input Nama dan NIM
+
+# Input nama depan
+nama_depan = input("Masukkan nama depan: ")
+
+# Input nama belakang
+nama_belakang = input("Masukkan nama belakang: ")
+
+# Gabungkan nama depan dan nama belakang
+nama_lengkap = nama_depan + " " + nama_belakang
+
+# Input NIM
+nim = input("Masukkan NIM: ")
+
+# Cetak output
+print("=========================================")
+print("Pendidikan Teknik Informatika")
+print("----------------------------------------")
+print("Nama: " + nama_lengkap.upper())
+print("NIM: " + nim)
+print("----------------------------------------")
+print("Fakultas Keguruan dan Ilmu Pendidikan")
+print("UNIVERSITAS MUHAMMADIYAH SURAKARTA")
+print("=========================================")
