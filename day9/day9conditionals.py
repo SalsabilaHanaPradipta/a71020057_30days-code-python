0a70fea319b8bf6fbaa1fe2a748cd9e962c75f90
SalsabilaHanaPradipta <a710230057@student.ums.ac.id>
2024-06-14 19:01:30 +0700

    Create day9conditionals.py
    day9conditionals

---
+++ day9/day9conditionals.py
@@ -0,0 +1,13 @@
+# Contoh if-else sederhana di Python
+nilai = 85
+
+if nilai >= 90:
+    print("Nilai Anda A")
+elif nilai >= 80:
+    print("Nilai Anda B")
+elif nilai >= 70:
+    print("Nilai Anda C")
+elif nilai >= 60:
+    print("Nilai Anda D")
+else:
+    print("Nilai Anda E")
